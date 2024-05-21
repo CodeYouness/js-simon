592a1b122bbde6375f3f712d89cd332695d9f255
CodeYouness <ylijassi@gmail.com>
2024-05-21 16:11:09 +0200

    added random number generator

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@
 
 <p>25</p>*/
 const numbersContainer = document.querySelector('main')
+let blacklist = []
 
+for (let i = 0; i < 5; i++) {
+    let numberEl = document.createElement('p')
+    const casualNumber = Math.floor(Math.random() * 100)
+    if (!blacklist.includes(casualNumber)) {
+        numberEl.textContent = casualNumber
+        numbersContainer.appendChild(numberEl)
+    }
+}
