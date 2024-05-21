dee1efb2b67f3e6d8eff05f4a79819616814916a
CodeYouness <ylijassi@gmail.com>
2024-05-21 16:35:34 +0200

    removed .append from inside the while to outside

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@
 
 <p>25</p>*/
 const numbersContainer = document.querySelector('main')
-let blacklist = []
+const numberEl = document.createElement('p')
+let blacklistComputer = []
+let blacklistUser = []
 
-while (blacklist.length < 5) {
-    let numberEl = document.createElement('p')
+while (blacklistComputer.length < 5) {
     const casualNumber = Math.floor(Math.random() * 100)
-    if (blacklist.includes(casualNumber) === false) {
-        numberEl.textContent = casualNumber
-        numbersContainer.appendChild(numberEl)
-        blacklist.push(casualNumber)
+    if (blacklistComputer.includes(casualNumber) === false) {
+        blacklistComputer.push(casualNumber)
     }
-    console.log(casualNumber)
 }
+
+numberEl.textContent = blacklistComputer.join('-')
+numbersContainer.append(numberEl)
+
+
+setTimeout(function () {
+    const rightNumber = []
+    numbersContainer.innerHTML = ''
+    while (blacklistUser.length < 5) {
+        const userNumber = Number.parseInt(prompt('whats the number displayed?'), 10)
+        if (true) {
+
+        }
+    }
+}, 3000)
